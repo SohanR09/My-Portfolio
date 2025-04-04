5ff5aab00033ba883f39a145e7a2ac271cfea185
Sohan Rathod <sohan1203@gmail.com>
2025-04-04 18:28:57 +0530

    key removed

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
 // Create a single supabase client for interacting with your database from the browser
-const supabaseUrl = process.env.SUPABASE_URL! || "https://okiadhwsjjlgjqmkxkim.supabase.co";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY! || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9raWFkaHdzampsZ2pxbWt4a2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NzMzMzcsImV4cCI6MjA1ODU0OTMzN30.5X6WazVpznQ7hLnfnkNCc4MifHz-U67gXy15DVyvRcA";
-
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
 // Create a singleton to prevent multiple instances
 let supabaseClient: ReturnType<typeof createClient> | null = null
 

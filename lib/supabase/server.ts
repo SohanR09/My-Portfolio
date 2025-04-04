a56f944f86f2449be4afcb0e5832d86261bdf67d
Sohan Rathod <sohan1203@gmail.com>
2025-04-04 18:30:55 +0530

    error

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 import { cookies } from "next/headers"
 
-const supabaseUrl = process.env.SUPABASE_URL! 
-  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.SUPABASE_URL! || "https://okiadhwsjjlgjqmkxkim.supabase.co";
+  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY! || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9raWFkaHdzampsZ2pxbWt4a2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NzMzMzcsImV4cCI6MjA1ODU0OTMzN30.5X6WazVpznQ7hLnfnkNCc4MifHz-U67gXy15DVyvRcA"
 // Create a Supabase client for server-side operations
 export const createServerSupabaseClient = () => {
   const cookieStore: any = cookies()
